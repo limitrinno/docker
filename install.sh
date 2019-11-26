8d62591eb1de2e90c4f59c56e3088b25317a960b
Limit_rinno <31068351+limitrinno@users.noreply.github.com>
2019-11-26 20:25:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ yum makecache
 yum remove docker docker-common docker-selinux docker-engine
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install -y docker-ce
 systemctl start docker
 systemctl enable docker
 docker --version
