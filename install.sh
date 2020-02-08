d24d78d0a283e5cd0bb4871fb147ceb42d2aecff
Limit_rinno <31068351+limitrinno@users.noreply.github.com>
2020-02-08 15:47:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-cd /etc/yum.repos.d/
-wget http://mirrors.163.com/.help/CentOS6-Base-163.repo
-yum clean all
-yum makecache
 yum remove docker docker-common docker-selinux docker-engine
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce
-systemctl start docker
-systemctl enable docker
-docker --version
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker && systemctl enable docker
+docker version
